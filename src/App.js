792ad25401c6fb57664f1bea4c03e2331f5c1d36
hellinay <f.helinay@gmail.com>
2022-03-21 14:45:57 +0300

    Add Form and FireBase Adaptation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-
   
   const [movies,setMovies] =useState([])
 
-  function fetchMoviesHandler(params) {
+  const [isLoading,setLoading]=useState(false)
 
-  fetch('https://swapi.dev/api/films').then(
-    (response) => {
-      return response.json();
-    }
-  ).then((data) =>{
-    const transformedData = data.results.map(movieData =>
+  const [error, setError]=useState(null)
+
+
+
+
+  const  fetchMoviesHandler = useCallback(async function() {
+
+    setLoading(true)
+    setError(null)
+    try {
+      const response= await fetch('https://react-http-db-51307-default-rtdb.firebaseio.com/movies.json')
+      const data = await response.json()
+
+      if(!response.ok){
+        throw new Error ('Something went wrong ')
+      }
+
+    const transformedData = data.results.map((movieData) =>
       {
         return{
           id:movieData.episode_id,
@@ -26,9 +37,31 @@ function App() {
         }
       })
       setMovies(transformedData)
-  }
-  )
+      setLoading(false)
+    } catch (error) {
+      setError(error.message)
+    }
+
     
+  },[])
+
+  useEffect(()=>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler])
+
+
+  let content = <p>Found no movies</p>
+  
+  if(movies.length>0){
+    content= <MoviesList movies={movies} />
+  }
+
+  if(error){
+    content =<p>{error}</p>
+  }
+
+  if(isLoading){
+    content=<p>Loading...</p>
   }
   
   return (
@@ -37,7 +70,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {content}
       </section>
     </React.Fragment>
   );
